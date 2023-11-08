35fb47741fd0efc02f8c4c34f05ccb7492cc25ba
Eugeny1978 <luchnik78@mail.ru>
2023-11-08 20:19:44 +0300

    Add Files

---
+++ data_bases/path_to_base.py
@@ -0,0 +1,12 @@
+import os
+
+# Имя Файла Базы Данных (БД)
+base_name = 'DataBase.db'
+# Папка данного файла. В этой же папке находится и файл БД
+folder = os.path.dirname(repr(__file__)).replace("'", '')
+
+# PATH = folder + '\\\\' + base_name # тоже работает, но предпочел чтобы с одной чертой
+path_to_file = (folder + '\\\\' + base_name)
+DATABASE = rf'{path_to_file}'.replace('\\\\', '/')
+
+# print(DATABASE)
