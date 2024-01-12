fa2157d034dcb679a0a5dbb4e16959a5424f2941
Eugeny1978 <luchnik78@mail.ru>
2024-01-12 19:52:51 +0300

    Modify Files

---
+++ bots/bot_neighbour.py
@@ -0,0 +1,27 @@
+import sqlite3 as sq                                 # Работа с БД
+import pandas as pd                                  # Объекты DataFrame
+from typing import Literal                           # Создание Классов Перечислений
+from time import sleep, time, localtime, strftime    # Создание технологических Пауз
+import random                                        # Случайные значения
+import seaborn as sns
+from matplotlib import pyplot as plt
+# import json
+
+
+# 0. Найти Точность
+# 1. Найти Среднюю цену
+#
+# 3. Стакан - найти Плиту
+# 4. Присоседиться Плитой
+# Найти 2ю Границу
+# 5. Размазать на участке
+#
+# Пересчет:
+# Стакан - плита осталась?
+# Пауза
+#
+# Сместилась
+# Пересчет на это смещение
+#
+# Нет Плит?
+# Вопрос
